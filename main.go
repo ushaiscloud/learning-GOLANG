ba4b55e12cac6102e24f62347b191ac2276f27d9
ushaiscloud <queenschoice101@gmail.com>
2025-10-23 00:59:35 +0100

    printalphabet

--- main.go
+++ main.go
@@ -2,32 +2,9 @@ package main
 
 import "fmt"
 
-func RetainFirstHalf(str string) string {
-	length := len(str)
-
-	// If string is empty, return empty string
-	if length == 0 {
-		return ""
-	}
-
-	// If string length is 1, return the string
-	if length == 1 {
-		return str
-	}
-
-	// Calculate half length (round down for odd numbers)
-	half := length / 2
-
-	// Return first half of the string
-	return str[:half]
-}
-
 func main() {
-	// Test cases
-	fmt.Println(RetainFirstHalf("Hello"))       // "He"
-	fmt.Println(RetainFirstHalf("World!"))      // "Wor"
-	fmt.Println(RetainFirstHalf("Go"))          // "G"
-	fmt.Println(RetainFirstHalf("A"))           // "A"
-	fmt.Println(RetainFirstHalf(""))            // ""
-	fmt.Println(RetainFirstHalf("Programming")) // "Progr"
+	for i := 'a'; i <= 'z'; i++ {
+		fmt.Print(string(i))
+	}
+	fmt.Println()
 }
