968a4d3d9d62e477653d1464ef85fad4d7973f16
ushaiscloud <queenschoice101@gmail.com>
2025-10-22 21:14:21 +0100

    printletters

---
+++ go.mod
@@ -0,0 +1,5 @@
+module ushaiscloud
+
+go 1.22.2
+
+require github.com/01-edu/z01 v0.2.0 // indirect
---
+++ main.go
@@ -0,0 +1,10 @@
+package main
+
+import "fmt"
+
+// this code actually prints the first four letter in the string//
+
+func main() {
+	str := "Ushaiscloud"
+	fmt.Println(str[:4])
+}
