6f2994b3a183344920e7ebcff80a1aa68c7c0703
ushaiscloud <queenschoice101@gmail.com>
2025-10-23 00:52:02 +0100

    retain first half

--- main.go
+++ main.go
@@ -2,10 +2,32 @@ package main
 
 import "fmt"
 
-// this progma prints the total letters in the string//
+func RetainFirstHalf(str string) string {
+	length := len(str)
+
+	// If string is empty, return empty string
+	if length == 0 {
+		return ""
+	}
+
+	// If string length is 1, return the string
+	if length == 1 {
+		return str
+	}
+
+	// Calculate half length (round down for odd numbers)
+	half := length / 2
+
+	// Return first half of the string
+	return str[:half]
+}
 
 func main() {
-	str := "Ushaiscloud"
-	count := len(str)
-	fmt.Println(count)
+	// Test cases
+	fmt.Println(RetainFirstHalf("Hello"))       // "He"
+	fmt.Println(RetainFirstHalf("World!"))      // "Wor"
+	fmt.Println(RetainFirstHalf("Go"))          // "G"
+	fmt.Println(RetainFirstHalf("A"))           // "A"
+	fmt.Println(RetainFirstHalf(""))            // ""
+	fmt.Println(RetainFirstHalf("Programming")) // "Progr"
 }
