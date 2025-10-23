75bf9fae14bd580e11ac2ebc2ff0fa239467ea24
ushaiscloud <queenschoice101@gmail.com>
2025-10-23 12:17:27 +0100

    PrintIfNot

--- main.go
+++ main.go
@@ -2,9 +2,19 @@ package main
 
 import "fmt"
 
-func main() {
-	for char := 'z'; char >= 'a'; char-- {
-		fmt.Printf("%c", char)
+func PrintIfNot(str string) string {
+
+	if len(str) >= 3 || str == "" {
+		return "G\n"
+
 	}
-	fmt.Println()
+
+	return "invalid Input\n"
+}
+
+func main() {
+	fmt.Print(PrintIfNot("abcdefz"))
+	fmt.Print(PrintIfNot("abc"))
+	fmt.Print(PrintIfNot(""))
+	fmt.Print(PrintIfNot("14"))
 }
